6b05831365c4fcdb3b43379c2519d42e565aa6a3
kirakira666 <847868144@qq.com>
2021-06-17 16:05:20 +0800

    Upload images.

--- frontend/muse/muse/lib/share_idea.dart
+++ frontend/muse/muse/lib/share_idea.dart
@@ -1,3 +1,7 @@
+import 'package:cloudbase_auth/cloudbase_auth.dart';
+import 'package:cloudbase_core/cloudbase_core.dart';
+import 'package:cloudbase_database/cloudbase_database.dart';
+import 'package:cloudbase_storage/cloudbase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:muse/storage_util.dart';
@@ -22,6 +26,7 @@ class ShareIdea extends StatefulWidget {
 }
 
 class _ShareIdeaState extends State<ShareIdea> {
+  var content = 'hhh';
   void _jumpDetailPage() {
     Navigator.push(
         context,
@@ -29,9 +34,9 @@ class _ShareIdeaState extends State<ShareIdea> {
           builder: (BuildContext context) => Detail(),
         ));
   }
+
   Future<void> _jumpLoginPage() async {
-    String token =
-    await StorageUtil.getStringItem('username');
+    String token = await StorageUtil.getStringItem('username');
     if (token != null) {
       // 跳转到首页
       print('yijdl');
@@ -41,8 +46,7 @@ class _ShareIdeaState extends State<ShareIdea> {
           gravity: ToastGravity.CENTER,
           backgroundColor: Colors.blue,
           textColor: Colors.white,
-          fontSize: 13.0
-      );
+          fontSize: 13.0);
     } else {
       // 跳转到登陆页面
       Navigator.push(
@@ -51,11 +55,10 @@ class _ShareIdeaState extends State<ShareIdea> {
             builder: (BuildContext context) => WelcomePage(),
           ));
     }
-
   }
+
   Future<void> _logout() async {
-    String token =
-    await StorageUtil.getStringItem('username');
+    String token = await StorageUtil.getStringItem('username');
     if (token != null) {
       // 跳转到首页
       print('yijdl');
@@ -67,8 +70,7 @@ class _ShareIdeaState extends State<ShareIdea> {
           gravity: ToastGravity.CENTER,
           backgroundColor: Colors.blue,
           textColor: Colors.white,
-          fontSize: 13.0
-      );
+          fontSize: 13.0);
     } else {
       Fluttertoast.showToast(
           msg: "当前处于未登陆状态！",
@@ -76,8 +78,7 @@ class _ShareIdeaState extends State<ShareIdea> {
           gravity: ToastGravity.CENTER,
           backgroundColor: Colors.blue,
           textColor: Colors.white,
-          fontSize: 13.0
-      );
+          fontSize: 13.0);
     }
   }
 
@@ -153,19 +154,23 @@ class _ShareIdeaState extends State<ShareIdea> {
           // ),
           Positioned(
             // child: _rootBack(),
-            top:MediaQuery.of(context).padding.top + 60,
+            top: MediaQuery.of(context).padding.top + 60,
             child: Column(
               children: <Widget>[
                 RaisedButton(
                   child: Text("拍照"),
                   onPressed: () => _getActionSheet(),
                 ),
+                RaisedButton(
+                  child: Text("提交"),
+                  onPressed: () => {_pushDB()},
+                ),
                 Text("----照片列表----"),
                 _imageList.isNotEmpty
                     ? Wrap(
-                  spacing: 10.0,
-                  children: _getImageList(),
-                )
+                        spacing: 10.0,
+                        children: _getImageList(),
+                      )
                     : Text("暂无内容")
               ],
             ),
@@ -213,7 +218,6 @@ class _ShareIdeaState extends State<ShareIdea> {
   //   });
   // }
 
-
   //图片列表的刻画
   List<Widget> _getImageList() {
     return _imageList.map((img) {
@@ -255,6 +259,7 @@ class _ShareIdeaState extends State<ShareIdea> {
       );
     }).toList();
   }
+
   //获取sheet选择
   Future _getActionSheet() async {
     await showModalBottomSheet(
@@ -282,6 +287,7 @@ class _ShareIdeaState extends State<ShareIdea> {
           );
         });
   }
+
   Future _getImage() async {
     Navigator.of(context).pop();
     var image = await ImagePicker.pickImage(
@@ -294,5 +300,87 @@ class _ShareIdeaState extends State<ShareIdea> {
       });
     }
   }
-}
 
+  Future<void> _pushDB() async {
+    CloudBaseCore core = CloudBaseCore.init({
+      // 填写您的云开发 env
+      'env': 'zhuji-cloudbase-3g9902drd47633ab',
+      // 填写您的移动应用安全来源凭证
+      // 生成凭证的应用标识必须是 Android 包名或者 iOS BundleID
+      'appAccess': {
+        // 凭证
+        'key': 'e6f33326a0d40fecfc67ffc2877255bc',
+        // 版本
+        'version': '1'
+      },
+      // 请求超时时间（选填）
+      'timeout': 3000
+    });
+    CloudBaseAuth auth = CloudBaseAuth(core);
+    CloudBaseDatabase db = CloudBaseDatabase(core);
+    CloudBaseStorage storage = CloudBaseStorage(core);
+    CloudBaseAuthState authState = await auth.getAuthState();
+    if (authState == null) {
+      await auth.signInAnonymously().then((success) {
+        // 登录成功
+        print('mm成功');
+      }).catchError((err) {
+        // 登录失败
+        print('mm失败');
+      });
+    }
+    String token = await StorageUtil.getStringItem('username');
+    var _ = db.command;
+    var o = {
+      'i':'ms',
+      '1':'hh'
+    };
+    var time = new DateTime.now().millisecondsSinceEpoch;
+    var urlL = [];
+    print(time);
+    for(int i = 0;i<_imageList.length;++i){
+      print(_imageList[i].path);
+      var filepath = _imageList[i].path;
+      var list = _imageList[i].path.split('/');
+      var filename = list[list.length-1];
+      print(filename);
+      var urlPath = 'userIdea/$token/$time/$i.png';
+      print(urlPath);
+      urlL.add(urlPath);
+      print(filepath);
+      await storage.uploadFile(
+          cloudPath: urlPath,
+          filePath: filepath,
+          onProcess: (int count, int total) {
+            // 当前进度
+            print(count);
+            // 总进度
+            print(total);
+          }
+      );
+      print('发表');
+    }
+    db.collection('idea').add({
+      'username': token,
+      'time': time,
+      'url': urlL,
+    }).then((res) {
+
+    }).catchError((e) {
+
+    });
+    // db.collection('user').where({
+    //   'username': token
+    // }).get().then((res) async {
+    //   var index = (res.data[0]['idea'].length).toString();
+    //   print(index);
+    //   content = content+'@@@&%'+index;
+    //   db.collection('user').where({'username': token}).update({
+    //     'data': _.push(content)
+    //   }).then((res) async {
+    //     print(res);
+    //
+    //   });
+    // });
+  }
+}
